d25aba9f80df3c32edc895f72f3a02e6f65e6ec4
Ayush Gupta <ayush.gupta.jk@gmail.com>
2022-01-25 14:56:58 +0530

    🏷️ added meta tags

---
+++ components/Layout.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const Layout = () => {
+	return (
+		<Head>
+			<meta charSet="UTF-8" />
+			<meta http-oequiv="X-UA-Compatible" content="IE=edge" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<title>Airpods Max</title>
+			<link rel="icon" href="favicon/favicon.ico" type="image/x-icon" />
+			<link rel="apple-touch-icon" href="favicon/favicon.png" />
+			<link rel="stylesheet" href="./styles/style.css" />
+
+		</Head>
+	);
+};
+
+export default Layout;
