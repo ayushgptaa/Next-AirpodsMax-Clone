c84eefd1c3538632ea4207f210102590af82652d
Ayush Gupta <ayush.gupta.jk@gmail.com>
2022-02-06 20:00:02 +0530

    🪠 Added Video component in index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Navbar from "components/Navbar";
 import Hero from "components/Hero";
+import Video from "components/Video";
 
 const Home = () => {
 	return (
 		<>
 			<Navbar />
 			<Hero />
+			<Video />
 		</>
 	);
 };
