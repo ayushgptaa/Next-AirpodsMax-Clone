8f39fb1f3028a6bce9c2e555e82c2a36f2f32f16
Ayush Gupta <ayush.gupta.jk@gmail.com>
2022-02-08 11:09:15 +0530

    🥼 Created TextSection component

---
+++ components/TextSection.js
@@ -0,0 +1,17 @@
+const TextSection = () => {
+	return (
+		<section className="section1">
+			<h3>Design</h3>
+			<h2>A radically origional composition.</h2>
+			<article>
+				<p>
+					The over-ear headphone has been completely reimagined. From cushion to canopy, AirPods Max are
+					designed for an uncompromising fit that creates the optimal acoustic seal for many different head
+					shapes — fully immersing you in every sound.
+				</p>
+			</article>
+		</section>
+	);
+};
+
+export default TextSection;
